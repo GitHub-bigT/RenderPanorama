edbeaeb3369f58013f334a4332976f3baf703bb7
GitHub-bigT <564241248@qq.com>
2017-09-16 19:45:16 +0800

    add 3rd

---
+++ RenderPanorama/RenderPanorama/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "3rd\include\glew\glew.h"
+#include "3rd\include\glfw\glfw3.h"
+#include "3rd\include\glm\glm.hpp"
+#include "3rd\include\glm\gtc\type_ptr.hpp"
+#include "3rd\include\glm\gtc\matrix_transform.hpp"
+
+#pragma comment (lib,"./3rd/lib/glew/glew32s.lib")
+#pragma comment (lib,"./3rd/lib/glfw/glfw3.lib")
+
+int main(){
+	
+}
